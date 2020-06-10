f4caa512ffbfab75213327c097a09d2ed0822613
Claire Buettner <claire@Claires-MacBook-Air.local>
2020-06-10 16:12:13 -0400

    seed is working need to make more restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Restaurants.destroy_all
-Reviews.destroy_all
-Users.destroy_all
+Restaurant.destroy_all
+Review.destroy_all
+User.destroy_all
 
-Users.create!(username: 'clairecodes', email: 'claire@claire.com', password: '123456')
+@user = User.create!(username: 'clairecodes', email: 'claire@claire.com', password: '123456')
 
-Reviews.create!(text: 'Leave your review here')
+@restaurant = Restaurant.create!(user: @user, name: 'Ugly Baby', imgURL: 'https://media.timeout.com/images/105556144/380/285/image.jpg', location: 'Carroll Gardens', description: 'Tucked away on a quiet stretch of Smith Street in Carroll Gardens is a Thai restaurant that will keep you coming back for more self-inflicted pain. Whether you’re ordering the “stay-away spicy Udon Thani’s duck salad” or the khao soi, the servers will warn you over and over to be careful of the spice. You’ll go against their advice and end up begging for more of the cooling cucumbers to ward off the heat.')
 
-Restaurants.create!(name: 'Ugly Baby', imgURL: 'https://media.timeout.com/images/105556144/380/285/image.jpg', location: 'Carroll Gardens', description: 'Tucked away on a quiet stretch of Smith Street in Carroll Gardens is a Thai restaurant that will keep you coming back for more self-inflicted pain. Whether you’re ordering the “stay-away spicy Udon Thani’s duck salad” or the khao soi, the servers will warn you over and over to be careful of the spice. You’ll go against their advice and end up begging for more of the cooling cucumbers to ward off the heat.')
+Review.create!(user: @user, restaurant: @restaurant, text: 'Leave your review here')
+
 
 # Restaurant.create!(name: '', imgURL: '', location: '', description: '')
 # Restaurant.create!(name: '', imgURL: '', location: '', description: '')
